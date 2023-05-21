347ecad0cee062de0a77e02a7bf3674c677565dc
Suraj Somani <surajsomani2002@gmail.com>
2023-05-21 19:26:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import{
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<News key= "general" pageSize={this.pageSize} country="in" category="general"/>}/>
+          <Route exact path="/Business" element={<News key= "Business" pageSize={this.pageSize} country="in" category="Business"/>}/>
+          <Route exact path="/Entertainment" element={<News key= "Entertainment" pageSize={this.pageSize} country="in" category="Entertainment"/>}/>
+          <Route exact path="/General" element={<News key= "General" pageSize={this.pageSize} country="in" category="General"/>}/>
+          <Route exact path="/Health" element={<News key= "Health" pageSize={this.pageSize} country="in" category="Health"/>}/>
+          <Route exact path="/Science" element={<News key= "Science" pageSize={this.pageSize} country="in" category="Science"/>}/>
+          <Route exact path="/Sports" element={<News key= "Sports" pageSize={this.pageSize} country="in" category="Sports"/>}/>
+          <Route exact path="/Technology" element={<News key= "Technology" pageSize={this.pageSize} country="in" category="Technology"/>}/>
+        </Routes>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+    return (
+      <div className='my-3'>
+        <div className="card">
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"style={{left:"90%", zIndex:"1"}}>{source}
+            <span className="visually-hidden"></span>
+          </span>
+          <img src={!imageUrl ? "https://static.tnn.in/thumb/msid-92374610,imgsize-100,width-1280,height-720,resizemode-75/92374610.jpg" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title} <span className="badge text-bg-success">New</span></h5>
+            <p className="card-text">{description}</p>
+            <p className='card-text'><small className='text-muted'>By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
